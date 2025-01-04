fd5d625837ff3b366a23f8ecafa4e9454d2dcf5c
github <durbaka945@gmail.com>
2025-01-04 12:04:47 +0530

    added model package

---
+++ src/main/java/nirmalyam/collection/model/Registration.java
@@ -0,0 +1,58 @@
+package com.nirmalyam.collection.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+    private String address;
+    private String schedule;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+}
